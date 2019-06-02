ccc2312938dfc4957e15b8df0cd3431da1cd7582
efrik1989 <efrik1989@rambler.ru>
2019-06-02 18:26:04 +0300

    version 1.0

--- app/src/main/java/com/example/k/simpelcalcandroid/Stack.java
+++ app/src/main/java/com/example/k/simpelcalcandroid/Stack.java
@@ -3,54 +3,47 @@ package com.example.k.simpelcalcandroid;
 import java.util.ArrayList;
 import java.util.List;
 
-//Класс Реализации Stack
-
-public class Stack {
+/**
+ * Класс Реализации Stack
+ */
+class Stack {
     private List<Object> list;
 
-    //Конструктор
-     Stack() {
+    Stack() {
         list = new ArrayList<>();
-
     }
 
-    //Добавить в стэк
-    public void addToStack(Object object) {
+    /**
+     * Добавить в стэк
+     */
+    void addToStack(Object object) {
         list.add(object);
     }
 
-    //Удалить из стэка
-    public void remove() {
+    /**
+     * Удалить из стэка
+     */
+    void remove() {
         list.remove(list.size() - 1);
 
     }
 
-    //Получение последнего добавленного элемента в стэк
-    public Object getTokenFromStack() {
+    /**
+     * Получение последнего добавленного элемента в стэк
+     */
+    Object getTokenFromStack() {
         if (list.size() != 0) {
             return list.get(list.size() - 1);
         }
         return null;
     }
 
-    //Проверка на пустоту стэка
-    public boolean IsEmpty() {
+    /**
+     * Проверка на пустой стек
+     */
+    boolean IsEmpty() {
         return list.size() == 0;
     }
-
-    // Проверка работаспособности стэка
-    public static void main(String[] args) {
-        Stack stack = new Stack();
-
-        stack.addToStack(";jgf");
-        stack.addToStack("жопа");
-        stack.addToStack("opa-popa");
-
-        while (!stack.IsEmpty()) {
-            System.out.println(stack.getTokenFromStack());
-            stack.remove();
-        }
-
-    }
 }
+
 
